dd0f90502ad2be941f6536d4a79b1f8c2bda3c73
AiMedHUB <128593290+aimedhub@users.noreply.github.com>
2024-08-03 16:47:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,6 +13,7 @@ let timestamp = 4102329600000;//2099-12-31
 //节点链接 + 订阅链接  https://dy.ssysub6.xyz/api/v1/client/subscribe?token=ec6df1627a600ae93db71d1cfd7f2941
 let MainData = `
 https://aivps.cc/api/v1/client/subscribe?token=04bb8a00cb934708d146062f05e787d3
+https://dy.ssysub6.xyz/api/v1/client/subscribe?token=ec6df1627a600ae93db71d1cfd7f2941
 `
 
 let urls = [];
